a932bea8fe728e8e3cfc978a9f31c274ca787f52
duchaoqun <du.chao.qun@163.com>
2019-07-24 04:17:54 +0800

    DemoJDBC

--- build.sbt
+++ build.sbt
@@ -1,2 +1,16 @@
 
 libraryDependencies += "org.postgresql" % "postgresql" % "42.2.5"
+
+
+
+// 在使用Jasper的时候需要用到如下包：
+// https://mvnrepository.com/artifact/com.lowagie/itext
+libraryDependencies += "com.lowagie" % "itext" % "2.1.7"
+// https://mvnrepository.com/artifact/net.sf.jasperreports/jasperreports
+libraryDependencies += "net.sf.jasperreports" % "jasperreports" % "6.8.0"
+
+// 引入一些外部包，在项目中创建一个lib目录，然后将这些包放到这里。
+unmanagedJars in Compile ++= Seq(
+  Attributed.blank[File](file(baseDirectory.value + "/lib/jcommon-1.0.23.jar")),
+  Attributed.blank[File](file(baseDirectory.value + "/lib/jfreechart-1.0.19.jar"))
+)
---
+++ src/info/aoye/DemoJDBC/DemoPostgres.scala
@@ -0,0 +1,23 @@
+package info.aoye.DemoJDBC
+
+import java.sql.{Connection, DriverManager, ResultSet}
+
+/**
+  * 链接Postgres数据库获取数据。
+  */
+object DemoPostgres extends App {
+  val strConn: String = "jdbc:postgresql://172.16.1.83:5432/lms_20190522?user=postgres&password=111111"
+  val conn = DriverManager.getConnection(strConn)
+
+  def selectObj(sql: String, conn: Connection): ResultSet = {
+    val prep = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)
+    val rs = prep.executeQuery(sql)
+    rs
+  }
+
+  val result1 = selectObj("select count(*) as aaa from account",conn)
+  while (result1.next()) {
+    println(result1.getString("aaa"))
+  }
+  conn.close()
+}
