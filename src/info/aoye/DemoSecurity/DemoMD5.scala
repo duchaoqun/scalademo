08e180e14559e2ede3ca6ae07a6a18a6d839f17c
duchaoqun <du.chao.qun@163.com>
2019-09-16 17:31:42 +0800

    update

--- src/info/aoye/DemoSecurity/DemoMD5.scala
+++ src/info/aoye/DemoSecurity/DemoMD5.scala
@@ -7,6 +7,10 @@ import info.aoye.util.Security
   */
 object DemoMD5 extends App {
 
-  val md5 = Security.computeHash("C:\\Users\\Chris\\Pictures\\2019091201.jpg")
+  val md5 = Security.computeFileMD5Hash("C:\\Users\\Chris\\Pictures\\2019091201.jpg")
   println(md5)
+
+  val strMd5 = Security.computeStringMD5Hash("root123")
+  println(strMd5)
+
 }
